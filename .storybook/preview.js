7ca21e5a08d973abce1841670f63f8e5bf2a6b42
habiburnobel <habiburnobel@gmail.com>
2022-06-27 17:39:28 +0600

    ThemeProvider added

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { addDecorator } from "@storybook/react";
-import Center from "../src/components/Center/Center";
+/* import { addDecorator } from "@storybook/react";
+import Center from "../src/components/Center/Center"; */
 
-addDecorator((story) => <Center>{story()}</Center>);
+import { ThemeProvider, theme, CSSReset, Box } from "@chakra-ui/react";
+
+// addDecorator((story) => <Center>{story()}</Center>);
 
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
@@ -19,3 +21,14 @@ export const parameters = {
         : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
   },
 };
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CSSReset />
+      <Box m="4">
+        <Story />
+      </Box>
+    </ThemeProvider>
+  ),
+];
